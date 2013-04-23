6309ce7e04b669c559f040a39a1387bafa066225
Kamil Misiowiec <mkakus@gmail.com>
2013-04-23 12:44:43 +0200

    Working console

--- src/console.cpp
+++ src/console.cpp
@@ -6,9 +6,37 @@ Console::Console(QWidget *parent) :
     ui(new Ui::Console)
 {
     ui->setupUi(this);
+
+    echo_ = true;
+    connect(ui->lineEdit, SIGNAL(returnPressed()),
+            this,         SLOT(returnPressed()));
 }
 
 Console::~Console()
 {
     delete ui;
 }
+
+void Console::setEchoEnable(bool value)
+{
+    echo_ = value;
+}
+
+void Console::write(const QString &msg)
+{
+    ui->plainTextEdit->appendPlainText(msg);
+}
+
+void Console::returnPressed()
+{
+    if (ui->lineEdit->text().length() == 0)
+        return;
+
+    if (echo_)
+        write(ui->lineEdit->text());
+
+    emit command(ui->lineEdit->text());
+
+    ui->lineEdit->clear();
+}
+
--- src/console.h
+++ src/console.h
@@ -14,9 +14,35 @@ class Console : public QDockWidget
 public:
     explicit Console(QWidget *parent = 0);
     ~Console();
+
+    /*!
+     * \brief if true then commands that was issued form command
+     *        line will be rewritten in the console window
+     * \param value true or false
+     */
+    void setEchoEnable(bool value);
+
+public slots:
+    /*!
+     * \brief Write message on the console
+     * \param msg Message that will be displayed
+     */
+    void write(const QString& msg);
+
+signals:
+    /*!
+     * \brief This signal is emited when user press enter, and
+     *        the command length is greater than 0.
+     * \param msg Message that user entered
+     */
+    void command(const QString &msg);
+
+private slots:
+    void returnPressed(void);
     
 private:
     Ui::Console *ui;
+    bool echo_;
 };
 
 #endif // CONSOLE_H
