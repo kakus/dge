4e05f6958d1c59e51aeedf497fa401c128f23b26
alibrandi <trzcinska.ania@gmail.com>
2013-05-01 20:37:26 +0200

    Engine class (QScriptEngine wrapper)

--- src/engine.cpp
+++ src/engine.cpp
@@ -1,5 +1,46 @@
 #include "engine.h"
 
+Engine* Engine::pInstance_;
+std::mutex Engine::mutex_;
+
 Engine::Engine()
 {
+}
+
+Engine::~Engine()
+{
+    delete pInstance_;
+}
+
+Engine* Engine::getInstance()
+{
+    if(!Engine::pInstance_)
+    {
+        mutex_.lock();
+        if(!pInstance_)
+            pInstance_ = new Engine;
+        mutex_.unlock();
+    }
+
+    return pInstance_;
+}
+
+
+void Engine::evaluate(const QString &code)
+{
+    QScriptValue result = engine_.evaluate(code);
+    emit evaluateResult(result);
+}
+
+void Engine::testTools(const QString& fileName, const QString& functionName) const
+{
+    QScriptValue function;
+
+    function = engine_.globalObject().property("tools").property(fileName).property(functionName);
+
+    if(!function.isFunction())
+        qDebug() << functionName << " is unknown for " << fileName << " script" << endl;
+    else
+        qDebug() << function.call().toString()<< endl;
+
 }
--- src/engine.h
+++ src/engine.h
@@ -1,10 +1,49 @@
+// singleton class to keep javasript interpreter
+// thread safe
+
 #ifndef ENGINE_H
 #define ENGINE_H
 
-class Engine
+#include <iostream>
+#include <mutex>
+#include <QScriptEngine>
+#include <QPushButton>
+#include <QDebug>
+
+//#include "loader.h"
+
+/*!
+ * \brief The Engine class
+ *  Singleton class wrapping javascript interpreter
+ *  Friend class Loader add preloaded tools to engine's global object before app start
+ */
+
+class Engine : public QObject
 {
 public:
+    static Engine* getInstance();
+    void testTools(const QString& fileName, const QString& functionName) const;
+    ~Engine();
+
+public slots:
+    void evaluate(const QString& code);
+
+signals:
+    void evaluateResult(const QScriptValue& code);
+
+private:
+
+    Q_OBJECT
+
+    QScriptEngine engine_;
+
+    static Engine* pInstance_;
+    static std::mutex mutex_;
+
     Engine();
+    Engine(const Engine&);
+
+    //friend class Loader;
 };
 
 #endif // ENGINE_H
