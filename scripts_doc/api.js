eb79bd0a0e611395f1d2118538b6ebd3d2e5b3a8
Kamil Misiowiec <mkakus@gmail.com>
2013-05-23 18:22:00 +0200

    update script doc

--- scripts_doc/api.js
+++ scripts_doc/api.js
@@ -1,16 +1,23 @@
 YUI.add("yuidoc-meta", function(Y) {
    Y.YUIDoc = { meta: {
     "classes": [
+        "BodyDef",
         "FixtureDef",
+        "console",
         "world"
     ],
     "modules": [
-        "core"
+        "core",
+        "utils"
     ],
     "allModules": [
         {
             "displayName": "core",
             "name": "core"
+        },
+        {
+            "displayName": "utils",
+            "name": "utils"
         }
     ]
 } };
